f389517b3d0e5680f1af141292a6aaeb53fadab2
AdrianPilko <50658445+AdrianPilko@users.noreply.github.com>
2021-09-17 16:10:15 +0100

    initial commit

---
+++ asmHelloBuild.bat
@@ -0,0 +1,5 @@
+% batch file file for compiling asmHello.asm
+
+del asm.tap
+
+e:\ZXSpectrum\asm\pasmo.exe -1 --tapbas asmHello.asm asm.tap
---
+++ asmTestBuild.bat
@@ -0,0 +1,5 @@
+% batch file file for compiling asmTest.asm
+
+del asm.tap
+
+e:\ZXSpectrum\asm\pasmo.exe -1 --tapbas asmTest.asm asm.tap
