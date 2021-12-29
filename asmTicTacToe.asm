799ed0b7cc3817b292798c80c316880bd0cefe24
AdrianPilko <50658445+AdrianPilko@users.noreply.github.com>
2021-12-29 09:47:28 +0000

    added initial logic for make move and

--- asmTicTacToe.asm
+++ asmTicTacToe.asm
@@ -27,9 +27,33 @@ CurrentYPos         equ $857f
 PreviousXPos        equ $8580
 PreviousYPos        equ $8581
 wasAMoveFlag        equ $8582
+whoseMoveIsIt		equ $8583  ;if zero then naughts, non zero crosses
 
 call start
 ret
+
+cross  defb 1,0,0,1,0,1,1,0,0,1,1,0,1,0,0,1
+naught defb 0,1,1,0,1,0,0,1,1,0,0,1,0,1,1,0
+
+drawNaughOrCross			; draw naught or cross at position CurrentYPos CurrentXPos
+	ld a, (whoseMoveIsIt)
+	cp a	; compare a, if zero then draw cross, if 1 draw naught
+	jp nz,drawNaught
+	ld a, 88
+	ld hl, CurrentYPos
+	ld d, (hl)
+	ld hl, CurrentXPos
+	ld e, (hl)
+	call Print_Char        	
+	ret
+drawNaught	
+	ld a, 55
+	ld hl, CurrentYPos
+	ld d, (hl)
+	ld hl, CurrentXPos
+	ld e, (hl)
+	call Print_Char        	
+	ret
 
 start:		
 	call clear_screen ; clear the screen and initialise screen stream
@@ -38,8 +62,12 @@ start:
 	ld de,gameTitleStr
 	ld bc,strLen 	
 	call print_text
-	ld a,boardColourAttr ; load a with board colour		
 
+	ld a,1					; crosses go first
+	ld (whoseMoveIsIt),a
+	
+	ld a,boardColourAttr ; load a with board colour		
+	
 	ld hl,screenStartHoriz_1 ; draw noughts and crosses (tic tac toe) criss-cross board	
 	ld b,16	
 loop_1: 		
@@ -69,11 +97,11 @@ loop_4:
 
 	; initialise the current x,y and the last x,y = current at this point
 	
-	ld a,(boardStoreXPos+4)
+	ld a,8
 	ld (CurrentXPos),a		
 	sub 1
 	ld (PreviousXPos), a
-	ld a,(boardStoreYPos+4)
+	ld a,4
 	ld (CurrentYPos),a
 	sub 1
 	ld (PreviousYPos), a
@@ -92,8 +120,8 @@ gameLoop:
 	ld (PreviousYPos), a
 	
 	ld a,0
-	ld (wasAMoveFlag),a
-
+	ld (wasAMoveFlag),a	
+	
 readKeyboard:	
 	call Read_Keyboard ; register A should now contain the key pressed first nibble is row, second is column
 	cp $50            ; 50 is row column on keyboard matrix for P
@@ -104,19 +132,16 @@ readKeyboard:
 	call z, moveUp
 	cp $5A            ; 5A is row column on keyboard matrix for Z
 	call z, moveDown		
-
-	; temporarily place n X or O, depending on whos go it is at the location no at
-	ld a,88
-	ld hl, CurrentYPos
-	ld d, (hl)
-	ld hl, CurrentXPos
-	ld e, (hl)
-	call Print_Char        
-
+	cp $4d            ; 
+	;call hprint
+	call z, makeMove		
+	
+	; temporarily place n X or O, depending on whos go it (whoseMoveIsIt) is at the location no at	
+	call drawNaughOrCross
+	
 	ld a, (wasAMoveFlag) ; load from memory where x position is stored
 	cp 0  ; check if flag was set
 	jp nz, clearPrevious	; if wasAMove zero then go back to game loop 
-	     
 	
 	jr gameLoop
 	
@@ -128,7 +153,9 @@ clearPrevious:	; clear the previous location
 	ld e, (hl)
 	call Print_Char
 	jr gameLoop
-	
+
+;CREDIT TO: 
+; http://www.breakintoprogram.co.uk/computers/zx-spectrum/keyboard
 Read_Keyboard:          LD HL,Keyboard_Map      ; Point HL at the keyboard list
                         LD D,8                  ; This is the number of ports (rows) to check
                         LD C,&FE                ; C is always FEh for reading keyboard ports
@@ -148,6 +175,14 @@ Read_Keyboard_1:        SRL A                   ; Shift A right; bit 0 sets carr
                         RET
 Read_Keyboard_2:        LD A,(HL)               ; We've found a key at this point; fetch the character code!
                         RET
+						
+makeMove
+	ld de,gameTitleStrMove  ; print debug to show Move character pressed
+	ld bc,strLenD 	
+	call print_text
+	ld a, (whoseMoveIsIt)
+	
+	ret
 
 moveRight:
 	ld a,1
@@ -158,9 +193,9 @@ moveRight:
 	
 	; this currently doesn't work the character doesn't move....
 	ld a, (CurrentXPos) ; load from memory where x position is stored
-	cp 31  ; limit x pos to width of screen
+	cp 20  ; limit x pos to width of screen
 	jp z,skipMR
-	add a,1			  ; increment the register a (x position = x position + 1)
+	add a,4			  ; increment the register a (x position = x position + 1)
 skipMR:	ld (CurrentXPos), a ; store the x position back to memory
 	ret
 moveLeft:	
@@ -170,9 +205,9 @@ moveLeft:
 	ld bc,strLenL 	
 	call print_text
 	ld a, (CurrentXPos) ; load from memory where x position is stored
-	cp 0	;limit x pos to minimum of left of screen (zero)
+	cp 8	;limit x pos to minimum of left of screen (zero)
 	jp z, skipML
-	sub 1			  ; decrement the register a (x position = x position - 1)	
+	sub 4			  ; decrement the register a (x position = x position - 1)	
 skipML:	ld (CurrentXPos), a ; store the x position back to memory
 	ret	
 moveUp:
@@ -182,9 +217,9 @@ moveUp:
 	ld bc,strLenU 	
 	call print_text
 	ld a, (CurrentYPos) ; load from memory where y position is stored
-	cp 0
+	cp 4
 	jp z, skipMU
-	sub 1			  ; increment the register a (y position = y position - 1)
+	sub 4			  ; increment the register a (y position = y position - 1)
 skipMU: ld (CurrentYPos), a ; store the y position back to memory
 	ret
 moveDown:	
@@ -194,16 +229,12 @@ moveDown:
 	ld bc,strLenD 	
 	call print_text
 	ld a, (CurrentYPos) ; load from memory where y position is stored	
-	cp 23
+	cp 16
 	jp z,skipMD
-	add a,1			  ; increment the register a (y position = y position + 1)
+	add a,4		  ; increment the register a (y position = y position + 1)
 skipMD: ld (CurrentYPos), a ; store the y position back to memory	
 	ret		
 clearKey:
-		
-drawCross:
-		ret
-drawCircle:
 		ret
 
 ; we store the board state in this block of memory		
@@ -227,6 +258,11 @@ strLenU			equ $ - gameTitleStrU
 gameTitleStrD	defb _at, 0, 0, _ink, 1, _paper, 6, _bright, 1, "   D  Noughts and Crosses      "
 strLenD			equ $ - gameTitleStrD
 
+gameTitleStrMove	defb _at, 0, 0, _ink, 1, _paper, 6, _bright, 1, "      Noughts and Crosses, Move"
+strLenMove			equ $ - gameTitleStrMove
+
+;CREDIT TO: 
+; http://www.breakintoprogram.co.uk/computers/zx-spectrum/keyboard
 Keyboard_Map    defb &FE,"#","Z","X","C","V"	
 				defb &FD,"A","S","D","F","G"
 				defb &FB,"Q","W","E","R","T"
@@ -262,7 +298,22 @@ Print_Char_L1:          LD A,(HL)               ; Get the byte from the ROM into
                         INC D                   ; Goto next line on screen
                         DJNZ Print_Char_L1      ; Loop around whilst it is Not Zero (NZ)
                         RET
- 
+
+hprint 	;; print 1byte hex		;;http://swensont.epizy.com/ZX81Assembly.pdf?i=1
+	PUSH AF ;store the original value of A for later
+	AND $F0 ; isolate the first digit
+	RRA
+	RRA
+	RRA
+	RRA
+	ADD A,$41 ; add 65 to the character code
+	CALL Print_Char ;
+	POP AF ; retrieve original value of A
+	AND $0F ; isolate the second digit
+	ADD A,$41 ; add 28 to the character code
+	CALL Print_Char		
+	RET
+	
 ; Get screen address from a character (X,Y) coordinate
 ; D = Y character position (0-23)
 ; E = X character position (0-31)
