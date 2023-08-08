df971e036ae9365a1867b51f6e369bc687f6df71
AdrianPilko <50658445+AdrianPilko@users.noreply.github.com>
2023-08-08 20:53:08 +0100

    changed to the other website that had no repeated rra later in pixel calc

--- asmPlotAtXY.asm
+++ asmPlotAtXY.asm
@@ -4,51 +4,40 @@ org $F000
 ;  c = x pixel position
 ;  hl stores the address
 ;  from:
-;  http://www.breakintoprogram.co.uk/hardware/computers/zx-spectrum/screen-memory-layout 
+;  https://www.overtakenbyevents.com/lets-talk-about-the-zx-specrum-screen-layout-part-three/
 pixelAddress:      
-    ld a,d          ; calculate y2,y1,y0
-    and %00000111   ; mask out unwanted bits
-    or %01000000    ; set base address of screen
-    ld h,a          ; store in h
-    ld a,d          ; calculate y7,y6
-    rra             ; shift to position
-    rra
-    rra
-    and %00011000   ; mask out unwanted bits
-    or h            ; or with y2,y1,y0
-    ld h,a          ; store in h
-    ld a,d          ; calculate y5,y4,y3
-    rla             ; shift to position
-    rla
-    and %11100000   ; mask out unwanted bits
-    ld l,a          ; store in l
-    ld a,c          ; calculate x4,x3,x2,x1,x0
-    rra             ; shift into position
-    rra
-    rra
-    and %00011111   ; mask out unwanted bits
-    or l            ; or with y5,y4,y3
-    ld l,a          ; store in l
+    ld a,d	; Work on the upper byte of the address
+    and %00000111		; a = Y2 Y1 y0
+    or %01000000		; first three bits are always 010
+    ld h,a		; store in h
+    ld a,d		; get bits Y7, Y6
+    rra		; move them into place
+    rra		;
+    rra		;
+    and %00011000		; mask off
+    or h		; a = 0 1 0 Y7 Y6 Y2 Y1 Y0
+    ld h,a		; calculation of h is now complete
+    ld a,d		; get y
+    rla		;
+    rla		;
+    and %11100000		; a = y5 y4 y3 0 0 0 0 0
+    ld l,a		; store in l
+    ld a,c		;
+    and %00011111		; a = X4 X3 X2 X1
+    or l		; a = Y5 Y4 Y3 X4 X3 X2 X1
+    ld l,a		; calculation of l is complete
     ret
     
 main:
 	call $0d6b ; rom routine to clear screen
-    ld b, 100
-    ld d, 20
-    ld c, 30    
+    ld b, 150
+    ld d, 5
+    ld c, 5    
 loop1:          ; loop1 draw vertical line
     call pixelAddress
-    ld (hl), 1        
+    ld (hl), 1
     inc d  
+    ;inc c
     djnz loop1
-    
-    ld b, 100
-    ld d, 120
-    ld c, 32   
-loop2:          ; loop2 draw horizontal line
-    call pixelAddress
-    ld (hl), $ff        
-    inc c
-    djnz loop2   
 	ret
 end main
