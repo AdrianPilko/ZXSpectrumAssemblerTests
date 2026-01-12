5fab904e60704ebde1957873d67199ec9dacee92
Adrian Pilkington <50658445+AdrianPilko@users.noreply.github.com>
2026-01-12 19:18:19 +0000

    Update fastPrintAt.asm

--- fastPrintAt.asm
+++ fastPrintAt.asm
@@ -11,31 +11,58 @@ ld hl, ATTR_T
 ld (hl),a
 ld hl, ATTR_S
 ld (hl), a
-
-call CLS
-
 ;set the screen boarder
 ld a, $03
 out ($fe),a
 
-ld b, $18-$0e       ; b = Y COORD
-ld c, $21-$03       ; b = X COORD
+ResetAndContinue:
+call CLS
+
+ld b, 24
+ld a, 24
+MainLoop:
+push bc
+    push af
+    call PrintMessage
+;    call DelayLoop
+    pop af 
+pop bc
+dec b
+dec a
+cp 0
+jp nz, MainLoop 
+jp ResetAndContinue
+ret
+
+
+PrintMessage:
+ld c, $21-$02       ; b = X COORD
 call LOCATE
 
 ld hl, msg
-
 Loop:
 ld a, (hl)
 or a
-jr z, Exit
+jr z, EndLoop
 rst $10
 inc hl
 jr Loop
+EndLoop:
+ret
 
+DelayLoop:
+ld b, $f0
+DelayLoop_1:
+    push bc
+    ld b, $f0
+DelayLoop_2:
+    djnz DelayLoop_2 
+    pop bc
+    djnz DelayLoop_1
+ret
 
-Exit:
-jr Exit
-
-msg:    defm 'Hello ZX Spectrum Assembly',$00
+             
+msg:    defm 'ByteForever ZX Spectrum Demo',$00
+row_to_print_at: defb $00
 
 end $8000
