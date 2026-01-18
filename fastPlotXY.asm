e146bf5643df520f627b2b8b89748f569567e298
Adrian Pilkington <50658445+AdrianPilko@users.noreply.github.com>
2026-01-18 08:31:02 +0000

    Update fastPlotXY.asm

--- fastPlotXY.asm
+++ fastPlotXY.asm
@@ -7,6 +7,8 @@ START_OF_ATTRIBUTE_SCREEN_MEM equ $5800
 Main:
     call CLS   ; clear screen rom routine using CLS equ
     call drawPlayAreaBorder
+    ld b, 10
+    ld c, 34
     call DrawSprite24x24 ;; this is just a test at momemt till it's working properly
 
     ld hl, $4849
@@ -130,43 +132,54 @@ SetColourLoop4:
 
 ;;010T TSSS LLLC CCCC
 DrawSprite24x24:   ; 3 by 3 character size sprite
-    ld hl, $4804   ;;;; just somewhere in centre third of screen vertically
+;; top left xy is in bc
+push bc
+    ;; the xy of first row is same as called by in bc
+    call GetScreenPos
+    ld a, 3
+    ;ld hl, $4804   ;;;; just somewhere in centre third of screen vertically
     ld de, Sprite1_24x24
-    call DrawHorizontalSprite_1x1
-
-    ld hl, $4805   ;;;; just somewhere in centre third of screen vertically
-    ld de, Sprite1_24x24+8
-    call DrawHorizontalSprite_1x1
-
-    ld hl, $4806   ;;;; just somewhere in centre third of screen vertically
-    ld de, Sprite1_24x24+16
-    call DrawHorizontalSprite_1x1
-   
-    ld hl, $4824   ;;;; just somewhere in centre third of screen vertically
+    call DrawHorizontalSprite_3wide
+pop bc
+push bc
+    ld a, c
+    add a, 8
+    ld c, a
+    call GetScreenPos
+    ld a, 3
+    ;ld hl, $4804   ;;;; just somewhere in centre third of screen vertically
     ld de, Sprite1_24x24+24
-    call DrawHorizontalSprite_1x1
-
-    ld hl, $4825   ;;;; just somewhere in centre third of screen vertically
-    ld de, Sprite1_24x24+32
-    call DrawHorizontalSprite_1x1
-
-    ld hl, $4826   ;;;; just somewhere in centre third of screen vertically
-    ld de, Sprite1_24x24+40
-    call DrawHorizontalSprite_1x1
-   
-    ld hl, $4844   ;;;; just somewhere in centre third of screen vertically
+    call DrawHorizontalSprite_3wide
+pop bc
+    ld a, c
+    add a, 16
+    ld c, a
+    call GetScreenPos
+    ld a, 3
+    ;ld hl, $4804   ;;;; just somewhere in centre third of screen vertically
     ld de, Sprite1_24x24+48
-    call DrawHorizontalSprite_1x1
+    call DrawHorizontalSprite_3wide
 
-    ld hl, $4845   ;;;; just somewhere in centre third of screen vertically
-    ld de, Sprite1_24x24+56
-    call DrawHorizontalSprite_1x1
-
-    ld hl, $4846   ;;;; just somewhere in centre third of screen vertically
-    ld de, Sprite1_24x24+64
-    call DrawHorizontalSprite_1x1
-   
 ret
+
+DrawHorizontalSprite_3wide:    
+    ld b, a    ; number of multiples of 8 blocks to display width
+MainLoopH1:
+    push bc
+        ld b, 8
+        push hl
+InnerLoopH1:
+            ld a, (de)
+            ld (hl), a
+            call NextScan
+            inc de
+            djnz InnerLoopH1
+        pop hl
+        inc l
+    pop bc
+    djnz MainLoopH1
+ret
+
 
 DrawHorizontalSprite_1x1
 ld b, 8
@@ -252,6 +265,50 @@ DelayLoop:
     pop af
     pop bc
 ret
+
+
+
+GetNextLine:
+	push af
+		ld hl,&0000
+ScreenLinePos_Plus2:
+		inc h
+		ld a,h
+		and  %00000111;7
+		jp nz,GetNextLineDone
+		ld a,l
+		add a,%00100000;32
+		ld l,a
+		jr c,GetNextLineDone
+		ld a,h
+		sub %00001000;8
+		ld h,a
+GetNextLineDone:
+	ld (ScreenLinePos_Plus2-2),hl
+	pop af
+	ret
+
+
+;;; from https://www.youtube.com/@ChibiAkumas
+;;https://www.youtube.com/watch?v=hGptSfPd2uA&list=WL&index=35
+GetScreenPos:	;return memory pos in HL of screen co-ord B,C (X,Y)
+	
+	push bc
+		ld b,0			;Load B with 0 because we only want C
+		ld hl,scr_addr_table
+		add hl,bc	;We add twice, because each address has 2 bytes
+		add hl,bc
+
+		ld a,(hl)	
+		inc l		;INC L not INC HL because we're byte aligned to 2
+		ld h,(hl)
+		ld l,a
+	pop bc
+	ld c,b		;Load the Xpos into C
+	ld b,&0	;Our table is relative to 0 - so we need to add our screen base
+	add hl,bc	;This is so it can be used for alt screen buffers
+	ld (ScreenLinePos_Plus2-2),hl
+	ret
 
 
 ;--------------------------------------------------
@@ -429,5 +486,32 @@ defb %00100000
 defb %00100000
 defb %11111111
 defb %00000000
+
+
+scr_addr_table:
+	dw &4000,&4100,&4200,&4300,&4400,&4500,&4600,&4700
+	dw &4020,&4120,&4220,&4320,&4420,&4520,&4620,&4720
+	dw &4040,&4140,&4240,&4340,&4440,&4540,&4640,&4740
+	dw &4060,&4160,&4260,&4360,&4460,&4560,&4660,&4760
+	dw &4080,&4180,&4280,&4380,&4480,&4580,&4680,&4780
+	dw &40A0,&41A0,&42A0,&43A0,&44A0,&45A0,&46A0,&47A0
+	dw &40C0,&41C0,&42C0,&43C0,&44C0,&45C0,&46C0,&47C0
+	dw &40E0,&41E0,&42E0,&43E0,&44E0,&45E0,&46E0,&47E0
+	dw &4800,&4900,&4A00,&4B00,&4C00,&4D00,&4E00,&4F00
+	dw &4820,&4920,&4A20,&4B20,&4C20,&4D20,&4E20,&4F20
+	dw &4840,&4940,&4A40,&4B40,&4C40,&4D40,&4E40,&4F40
+	dw &4860,&4960,&4A60,&4B60,&4C60,&4D60,&4E60,&4F60
+	dw &4880,&4980,&4A80,&4B80,&4C80,&4D80,&4E80,&4F80
+	dw &48A0,&49A0,&4AA0,&4BA0,&4CA0,&4DA0,&4EA0,&4FA0
+	dw &48C0,&49C0,&4AC0,&4BC0,&4CC0,&4DC0,&4EC0,&4FC0
+	dw &48E0,&49E0,&4AE0,&4BE0,&4CE0,&4DE0,&4EE0,&4FE0
+	dw &5000,&5100,&5200,&5300,&5400,&5500,&5600,&5700
+	dw &5020,&5120,&5220,&5320,&5420,&5520,&5620,&5720
+	dw &5040,&5140,&5240,&5340,&5440,&5540,&5640,&5740
+	dw &5060,&5160,&5260,&5360,&5460,&5560,&5660,&5760
+	dw &5080,&5180,&5280,&5380,&5480,&5580,&5680,&5780
+	dw &50A0,&51A0,&52A0,&53A0,&54A0,&55A0,&56A0,&57A0
+	dw &50C0,&51C0,&52C0,&53C0,&54C0,&55C0,&56C0,&57C0
+	dw &50E0,&51E0,&52E0,&53E0,&54E0,&55E0,&56E0,&57E0
 
 end $8000
