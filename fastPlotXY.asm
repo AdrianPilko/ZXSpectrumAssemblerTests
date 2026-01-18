a1d2f89620c27fb84808f6d53834ed63d2a34756
Adrian Pilkington <50658445+AdrianPilko@users.noreply.github.com>
2026-01-18 10:09:46 +0000

    added key input

--- fastPlotXY.asm
+++ fastPlotXY.asm
@@ -7,17 +7,97 @@ START_OF_ATTRIBUTE_SCREEN_MEM equ $5800
 Main:
     call CLS   ; clear screen rom routine using CLS equ
     call drawPlayAreaBorder
-    ld b, 10
-    ld c, 34
-    call DrawSprite24x24 ;; this is just a test at momemt till it's working properly
 
     ld hl, $4849
     ld de, GraphicTile2_8x8
     ld a, 12
     call DrawHorizontalBar
 
-    ld hl, $5050  ;; start address of platform at left end
-    xor a
+
+    ld a, 10
+    ld (SpriteXPos), a
+    ld a, 30
+    ld (SpriteYPos), a
+
+    ;ld hl, $5050  ;; start address of platform at left end
+    ;xor a
+
+
+    ;; read keys and make to allow what for with the sprite shall move    
+ScanTheKeyBoard:
+ScanKey_A:
+    ld d, $00
+    ld a, $fd 
+    in a, ($fe)
+    bit $00, a
+    jp nz, ScanKey_Z
+    set $00, d
+ScanKey_Z:
+    ld a, $fe
+    in a, ($fe)
+    bit $01, a
+    jr nz, ScanKey_O
+    set $01, d  
+ScanKey_O:
+    ld a, $cf
+    in a, ($fe)
+    bit $01, a
+    jr nz, ScanKey_P
+    set $02, d  
+ScanKey_P:
+    ld a, $df
+    in a, ($fe)
+    bit $00, a
+    jr nz, DoneScanKeys
+    set $03, d    
+
+
+DoneScanKeys:       
+    ; check if d bit zero set
+    bit $00, d
+    jr nz, MoveSpriteUp
+    bit $01, d
+    jr nz, MoveSpriteDown
+    bit $02, d
+    jr nz, MoveSpriteLeft
+    bit $03, d
+    jr nz, MoveSpriteRight
+    jr DrawSprite
+
+
+MoveSpriteUp:       ;; no limit checking yet!
+    ld a, (SpriteYPos)
+    dec a
+    ld (SpriteYPos), a
+    jr DrawSprite
+MoveSpriteDown:
+    ld a, (SpriteYPos)
+    inc a
+    ld (SpriteYPos), a
+MoveSpriteLeft:       ;; no limit checking yet!
+    ld a, (SpriteXPos)
+    dec a
+    ld (SpriteXPos), a
+    jr DrawSprite
+MoveSpriteRight:
+    ld a, (SpriteXPos)
+    inc a
+    ld (SpriteXPos), a
+
+DrawSprite:
+    ld a, (SpriteXPos)
+    ld b, a
+    ld a, (SpriteYPos)
+    ld c, a 
+ 
+    call DrawSprite24x24 ;; this is just a test at momemt till it's working properly
+
+    ;jp ScanTheKeyBoard
+    jp ScanTheKeyBoard
+
+
+
+;; effectively commented out code to draw a moving platform
 MovingPlatformLoop:
     push af
         ld a, 3
@@ -400,6 +480,40 @@ GraphicTileBlank_8x8:    ; a box empty for no attribute colour
     defb %00000000
     defb %00000000
 
+
+SpriteXPos:
+    defb 10
+SpriteYPos:
+    defb 30
+
+
+scr_addr_table:
+	dw &4000,&4100,&4200,&4300,&4400,&4500,&4600,&4700
+	dw &4020,&4120,&4220,&4320,&4420,&4520,&4620,&4720
+	dw &4040,&4140,&4240,&4340,&4440,&4540,&4640,&4740
+	dw &4060,&4160,&4260,&4360,&4460,&4560,&4660,&4760
+	dw &4080,&4180,&4280,&4380,&4480,&4580,&4680,&4780
+	dw &40A0,&41A0,&42A0,&43A0,&44A0,&45A0,&46A0,&47A0
+	dw &40C0,&41C0,&42C0,&43C0,&44C0,&45C0,&46C0,&47C0
+	dw &40E0,&41E0,&42E0,&43E0,&44E0,&45E0,&46E0,&47E0
+	dw &4800,&4900,&4A00,&4B00,&4C00,&4D00,&4E00,&4F00
+	dw &4820,&4920,&4A20,&4B20,&4C20,&4D20,&4E20,&4F20
+	dw &4840,&4940,&4A40,&4B40,&4C40,&4D40,&4E40,&4F40
+	dw &4860,&4960,&4A60,&4B60,&4C60,&4D60,&4E60,&4F60
+	dw &4880,&4980,&4A80,&4B80,&4C80,&4D80,&4E80,&4F80
+	dw &48A0,&49A0,&4AA0,&4BA0,&4CA0,&4DA0,&4EA0,&4FA0
+	dw &48C0,&49C0,&4AC0,&4BC0,&4CC0,&4DC0,&4EC0,&4FC0
+	dw &48E0,&49E0,&4AE0,&4BE0,&4CE0,&4DE0,&4EE0,&4FE0
+	dw &5000,&5100,&5200,&5300,&5400,&5500,&5600,&5700
+	dw &5020,&5120,&5220,&5320,&5420,&5520,&5620,&5720
+	dw &5040,&5140,&5240,&5340,&5440,&5540,&5640,&5740
+	dw &5060,&5160,&5260,&5360,&5460,&5560,&5660,&5760
+	dw &5080,&5180,&5280,&5380,&5480,&5580,&5680,&5780
+	dw &50A0,&51A0,&52A0,&53A0,&54A0,&55A0,&56A0,&57A0
+	dw &50C0,&51C0,&52C0,&53C0,&54C0,&55C0,&56C0,&57C0
+	dw &50E0,&51E0,&52E0,&53E0,&54E0,&55E0,&56E0,&57E0
+
+
 Sprite1_24x24  
  ; beacuse of the way the sprite is being drawn this has to be organised in memory the same way
  ; in other words all the first column of pixels of the first 8bytes then all the second and so on
@@ -488,30 +602,6 @@ defb %11111111
 defb %00000000
 
 
-scr_addr_table:
-	dw &4000,&4100,&4200,&4300,&4400,&4500,&4600,&4700
-	dw &4020,&4120,&4220,&4320,&4420,&4520,&4620,&4720
-	dw &4040,&4140,&4240,&4340,&4440,&4540,&4640,&4740
-	dw &4060,&4160,&4260,&4360,&4460,&4560,&4660,&4760
-	dw &4080,&4180,&4280,&4380,&4480,&4580,&4680,&4780
-	dw &40A0,&41A0,&42A0,&43A0,&44A0,&45A0,&46A0,&47A0
-	dw &40C0,&41C0,&42C0,&43C0,&44C0,&45C0,&46C0,&47C0
-	dw &40E0,&41E0,&42E0,&43E0,&44E0,&45E0,&46E0,&47E0
-	dw &4800,&4900,&4A00,&4B00,&4C00,&4D00,&4E00,&4F00
-	dw &4820,&4920,&4A20,&4B20,&4C20,&4D20,&4E20,&4F20
-	dw &4840,&4940,&4A40,&4B40,&4C40,&4D40,&4E40,&4F40
-	dw &4860,&4960,&4A60,&4B60,&4C60,&4D60,&4E60,&4F60
-	dw &4880,&4980,&4A80,&4B80,&4C80,&4D80,&4E80,&4F80
-	dw &48A0,&49A0,&4AA0,&4BA0,&4CA0,&4DA0,&4EA0,&4FA0
-	dw &48C0,&49C0,&4AC0,&4BC0,&4CC0,&4DC0,&4EC0,&4FC0
-	dw &48E0,&49E0,&4AE0,&4BE0,&4CE0,&4DE0,&4EE0,&4FE0
-	dw &5000,&5100,&5200,&5300,&5400,&5500,&5600,&5700
-	dw &5020,&5120,&5220,&5320,&5420,&5520,&5620,&5720
-	dw &5040,&5140,&5240,&5340,&5440,&5540,&5640,&5740
-	dw &5060,&5160,&5260,&5360,&5460,&5560,&5660,&5760
-	dw &5080,&5180,&5280,&5380,&5480,&5580,&5680,&5780
-	dw &50A0,&51A0,&52A0,&53A0,&54A0,&55A0,&56A0,&57A0
-	dw &50C0,&51C0,&52C0,&53C0,&54C0,&55C0,&56C0,&57C0
-	dw &50E0,&51E0,&52E0,&53E0,&54E0,&55E0,&56E0,&57E0
+
 
 end $8000
